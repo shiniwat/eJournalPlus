306741d8da24cea61b638453ab9f8be45b1ae927
SND\shiniwa_cp <SND\shiniwa_cp@e17a0e51-4ae3-4d35-97c3-1a29b211df97>
2009-04-06 06:15:30 +0000

    Added server setup tool (ServerSetup.exe) into server solution.
    git-svn-id: https://ejournalplus.svn.codeplex.com/svn@22865 e17a0e51-4ae3-4d35-97c3-1a29b211df97

---
+++ server/EjsPublicService/ServerSetup/ServerSetupForm.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace ServerSetup
+{
+    partial class ServerSetupForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ServerSetupForm));
+            this._cancelButton = new System.Windows.Forms.Button();
+            this._configureButton = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this._virtualEndPoint = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this._serviceName = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this._adminPassword = new System.Windows.Forms.TextBox();
+            this._adminName = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this._serverName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // _cancelButton
+            // 
+            resources.ApplyResources(this._cancelButton, "_cancelButton");
+            this._cancelButton.Name = "_cancelButton";
+            this._cancelButton.UseVisualStyleBackColor = true;
+            this._cancelButton.Click += new System.EventHandler(this._cancelButton_Click);
+            // 
+            // _configureButton
+            // 
+            resources.ApplyResources(this._configureButton, "_configureButton");
+            this._configureButton.Name = "_configureButton";
+            this._configureButton.UseVisualStyleBackColor = true;
+            this._configureButton.Click += new System.EventHandler(this._configureButton_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this._virtualEndPoint);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this._serviceName);
+            resources.ApplyResources(this.groupBox2, "groupBox2");
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.TabStop = false;
+            // 
+            // _virtualEndPoint
+            // 
+            resources.ApplyResources(this._virtualEndPoint, "_virtualEndPoint");
+            this._virtualEndPoint.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this._virtualEndPoint.Name = "_virtualEndPoint";
+            // 
+            // label5
+            // 
+            resources.ApplyResources(this.label5, "label5");
+            this.label5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.label5.Name = "label5";
+            // 
+            // label4
+            // 
+            resources.ApplyResources(this.label4, "label4");
+            this.label4.Name = "label4";
+            // 
+            // _serviceName
+            // 
+            resources.ApplyResources(this._serviceName, "_serviceName");
+            this._serviceName.Name = "_serviceName";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this._adminPassword);
+            this.groupBox1.Controls.Add(this._adminName);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this._serverName);
+            this.groupBox1.Controls.Add(this.label1);
+            resources.ApplyResources(this.groupBox1, "groupBox1");
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.TabStop = false;
+            // 
+            // _adminPassword
+            // 
+            resources.ApplyResources(this._adminPassword, "_adminPassword");
+            this._adminPassword.Name = "_adminPassword";
+            // 
+            // _adminName
+            // 
+            resources.ApplyResources(this._adminName, "_adminName");
+            this._adminName.Name = "_adminName";
+            // 
+            // label3
+            // 
+            resources.ApplyResources(this.label3, "label3");
+            this.label3.Name = "label3";
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.Name = "label2";
+            // 
+            // _serverName
+            // 
+            resources.ApplyResources(this._serverName, "_serverName");
+            this._serverName.Name = "_serverName";
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            // 
+            // ServerSetupForm
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this._cancelButton);
+            this.Controls.Add(this._configureButton);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.MaximizeBox = false;
+            this.Name = "ServerSetupForm";
+            this.Load += new System.EventHandler(this.ServerSetupForm_Load);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button _cancelButton;
+        private System.Windows.Forms.Button _configureButton;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label _virtualEndPoint;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox _serviceName;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox _adminPassword;
+        private System.Windows.Forms.TextBox _adminName;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox _serverName;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ server/EjsPublicService/ServerSetup/ServerSetupForm.cs
@@ -0,0 +1,316 @@
+﻿///	-----------------------------------------------------------------
+///	ServerConfigForm.cs: main form code behind.
+///	Copyright shiniwa, 2009, All Rights Reserved.
+///	current owner : shiniwa
+///	-----------------------------------------------------------------
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.ServiceProcess;
+using System.Configuration;
+using System.Xml.Linq;
+using System.IO;
+using System.Data.SqlClient;
+using System.Threading;
+
+namespace ServerSetup
+{
+    internal enum sqlCheckResult
+    {
+        Invalid,
+        Okay,
+        BypassSqlSetup,
+        UserCancel,
+        Unknown,
+    }
+
+    public partial class ServerSetupForm : Form
+    {
+        public ServerSetupForm()
+        {
+            InitializeComponent();
+        }
+
+        private void _cancelButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        /// <summary>
+        /// Event handler that gets called when the form gets loaded.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ServerSetupForm_Load(object sender, EventArgs e)
+        {
+            // populate this machine name as the default server name
+            string machineName = Environment.MachineName;
+            string serverName = machineName;
+            bool foundService = false;
+            // Let's see if SQLSERVER service exist on this machine
+            //	Check to see SQLSERVER service
+            try
+            {
+                ServiceController scSqlSrv = new ServiceController("SQLSERVER");
+                if (scSqlSrv.Status == ServiceControllerStatus.Running)
+                {
+                    //	OK
+                    foundService = true;
+                }
+            }
+            catch (Win32Exception we)
+            {
+                System.Diagnostics.Debug.WriteLine(we.Message);
+            }
+            catch (InvalidOperationException ie)
+            {
+                System.Diagnostics.Debug.WriteLine(ie.Message);
+            }
+
+            if (!foundService)
+            {
+                try
+                {
+                    ServiceController scSqlExpress = new ServiceController("MSSQL$SQLEXPRESS");
+                    if (scSqlExpress.Status == ServiceControllerStatus.Running)
+                    {
+                        serverName += "\\SQLEXPRESS";
+                        foundService = true;
+                    }
+                }
+                catch (Win32Exception we)
+                {
+                    System.Diagnostics.Debug.WriteLine(we.Message);
+                }
+                catch (InvalidOperationException ie)
+                {
+                    System.Diagnostics.Debug.WriteLine(ie.Message);
+                }
+            }
+            this._serverName.Text = serverName;
+            this._adminName.Text = "sa";
+            this._serviceName.Text = machineName + ":80";
+            this._virtualEndPoint.Text = ConfigurationManager.AppSettings["virtualEndPoint"];
+        }
+
+        /// <summary>
+        /// Verify sql parameters user specified.
+        /// </summary>
+        /// <returns></returns>
+        private sqlCheckResult VerifySqlParam()
+        {
+            sqlCheckResult result = sqlCheckResult.Unknown;
+            //	let's see if SqlConnection is correct.
+            SqlConnection connection = new SqlConnection();
+            connection.ConnectionString = string.Format("Server={0};User Id={1};Password={2};Connection Timeout=20;",
+                                                _serverName.Text, _adminName.Text, _adminPassword.Text);
+            try
+            {
+                connection.Open();
+                string dbName = ConfigurationManager.AppSettings["databaseName"];
+                if (IsDbExists(connection, dbName))
+                {
+                    // DB exists.
+                    DialogResult dr = MessageBox.Show("The target dababase exists already\nClick Yes to bypass the database setup, No to cancel the whole process", "Server Setup", MessageBoxButtons.YesNo);
+                    if (dr == DialogResult.Yes)
+                    {
+                        result = sqlCheckResult.BypassSqlSetup;
+                    }
+                    else
+                    {
+                        result = sqlCheckResult.UserCancel;
+                    }
+                }
+                else
+                {
+                    result = sqlCheckResult.Okay;
+                }
+                connection.Close();
+            }
+            catch (SqlException se)
+            {
+                System.Diagnostics.Debug.WriteLine(se.Message);
+                MessageBox.Show(se.Message);
+                result = sqlCheckResult.Invalid;
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// Run the script where the configuration specified.
+        /// </summary>
+        /// <returns></returns>
+        private bool RunScript()
+        {
+            string currentDir = ConfigurationManager.AppSettings["scriptRunDir"];
+            string prevCD = System.Environment.CurrentDirectory;
+            if (!string.IsNullOrEmpty(currentDir))
+            {
+                string fullPath = Path.GetFullPath(currentDir);
+                if (fullPath != null)
+                {
+                    System.Environment.CurrentDirectory = fullPath;
+                }
+            }
+            System.Environment.CurrentDirectory = currentDir;
+            string runExe = ConfigurationManager.AppSettings["runScriptExe"];
+            string runFormat = ConfigurationManager.AppSettings["runScriptParam"];
+            if (!string.IsNullOrEmpty(runExe) && !string.IsNullOrEmpty(runFormat))
+            {
+                string runParam = string.Format(runFormat, _serverName.Text, _adminName.Text, _adminPassword.Text);
+                System.Diagnostics.Process proc = System.Diagnostics.Process.Start(runExe, runParam);
+                proc.WaitForExit();
+                System.Environment.CurrentDirectory = prevCD;
+                return true;
+            }
+            System.Environment.CurrentDirectory = prevCD;
+            return false;
+        }
+
+        /// <summary>
+        /// Check to see if the target database exists already.
+        /// </summary>
+        /// <param name="connection"></param>
+        /// <param name="dbName"></param>
+        /// <returns></returns>
+        private bool IsDbExists(SqlConnection connection, string dbName)
+        {
+            string sql = string.Format("select * from {0}.sys.objects where type_desc = 'USER_TABLE'", dbName);
+            SqlCommand command = new SqlCommand(sql, connection);
+            bool exists = false;
+            try
+            {
+                SqlDataReader reader = command.ExecuteReader();
+                if (reader.Read())
+                {
+                    exists = true;
+                }
+                reader.Close();
+            }
+            catch (SqlException se)
+            {
+                System.Diagnostics.Debug.WriteLine(se.Message);
+                //  probably, the database does not exist.
+            }
+            return exists;
+        }
+
+        /// <summary>
+        /// Event handler when user pressed setup button.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void _configureButton_Click(object sender, EventArgs e)
+        {
+            sqlCheckResult result = VerifySqlParam();
+            if (result == sqlCheckResult.Invalid || result == sqlCheckResult.UserCancel || result == sqlCheckResult.Unknown)
+            {
+                return;
+            }
+
+            //  make sure the target db does not exist.
+            //  and bypass if exists
+            if (result != sqlCheckResult.BypassSqlSetup)
+            {
+                bool runOK = RunScript();
+                if (!runOK)
+                {
+                    return;
+                }
+            }
+
+            string dbName = ConfigurationManager.AppSettings["databaseName"];
+            string targetConfig = ConfigurationManager.AppSettings["configFileName"];
+            if (!string.IsNullOrEmpty(targetConfig))
+            {
+                XElement root = XElement.Load(targetConfig);
+                if (root != null)
+                {
+                    bool modified = false;
+                    XElement settingElement = root.Element("appSettings");
+                    if (settingElement != null)
+                    {
+                        foreach (XElement child in settingElement.Elements())
+                        {
+                            if (child.Name == "add")
+                            {
+                                XAttribute keyAttr = child.FirstAttribute;
+                                if (keyAttr != null && keyAttr.Name == "key" && keyAttr.Value == "baseAddress")
+                                {
+                                    XAttribute valAttr = keyAttr.NextAttribute;
+                                    if (valAttr != null)
+                                    {
+                                        valAttr.Value = string.Format("http://{0}/EjsWcfSvc/EjsPublic.svc", _serviceName.Text);
+                                        modified = true;
+                                    }
+                                }
+                                else if (keyAttr != null && keyAttr.Name == "key" && keyAttr.Value == "connectionString")
+                                {
+                                    XAttribute valAttr = keyAttr.NextAttribute;
+                                    if (valAttr != null)
+                                    {
+                                        valAttr.Value = string.Format("Server={0};Database={1};User Id={2};Password={3};Connection Timeout=90;",
+                                                _serverName.Text, dbName, _adminName.Text, _adminPassword.Text);
+                                        modified = true;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    if (modified)
+                    {
+                        root.Save("temp.xml");
+                        root = null;
+                        File.Copy("temp.xml", targetConfig, true);
+                        //  and delete the temp
+                        File.Delete("temp.xml");
+                    }
+
+                    RunWCFReg();
+
+                    //  @todo: run MSI setup here.
+                    RunMsiSetup();
+
+					//	Very weired, but anyhow, we may need to run WCFReg twice...
+					RunWCFReg();
+					
+					//  OK finished.
+                    //MessageBox.Show("Server setup finished");
+                    //  delay the exit somewhat
+                    
+                    this.Close();
+                }
+            }
+        }
+
+        private void RunWCFReg()
+        {
+            //  this is supposed to be C:\Windows\Microsoft.NET\Framework\v3.0\Windows Communication Foundation\ServiceModelReg.exe"
+            string systemFolder = Environment.GetFolderPath(Environment.SpecialFolder.System);
+            string targetDir = string.Format(@"{0}\..\Microsoft.NET\Framework\v3.0\Windows Communication Foundation\", systemFolder);
+            targetDir = Path.GetFullPath(targetDir);
+            string targetExe = Path.Combine(targetDir, "ServiceModelReg.exe");
+            System.Diagnostics.Process proc = System.Diagnostics.Process.Start(targetExe, "-i");
+            proc.WaitForExit();
+        }
+
+        /// <summary>
+        /// Run MSI setup
+        /// </summary>
+        private void RunMsiSetup()
+        {
+            string runExe = ConfigurationManager.AppSettings["runMsiSetup"];
+            if (!string.IsNullOrEmpty(runExe))
+            {
+                System.Diagnostics.Process proc = System.Diagnostics.Process.Start(runExe);
+                proc.WaitForExit();
+            }
+            Thread.Sleep(1000);
+        }
+    }
+}
