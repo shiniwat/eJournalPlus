a02853620cbda0eb8af004825348cb5f88fdc1c9
SND\shiniwa_cp <SND\shiniwa_cp@e17a0e51-4ae3-4d35-97c3-1a29b211df97>
2015-06-18 08:39:10 +0000

    Added new ClickOnce stuff integrated with Windows Azure.
    git-svn-id: https://ejournalplus.svn.codeplex.com/svn@96668 e17a0e51-4ae3-4d35-97c3-1a29b211df97

---
+++ client/Meet E Journal/ejpClickOnceInstall/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ejpClickOnceInstall.MvcApplication" Language="C#" %>
---
+++ client/Meet E Journal/ejpClickOnceInstall/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ejpClickOnceInstall.Startup))]
+namespace ejpClickOnceInstall
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
